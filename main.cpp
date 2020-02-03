c31ac3c6e244f27ced5c5b6487a6a2e2eecf7723
peiyunluo <peiyunluo@icloud.com>
2020-02-03 12:11:03 +0800

    add max2 && max3

--- main.cpp
+++ main.cpp
@@ -31,12 +31,44 @@ void reverse(int *A, int lo, int hi) {
 }
 //TODO
 //实现max2
+/*
+ * Max2:迭代2
+ * */
+void max2(int A[], int lo, int hi, int &x1, int &x2) {
+    if (A[x1 = lo] < A[x2 = lo + 1])swap(x1, x2);
+    for (int i = lo + 2; i < hi; i++) {
+        if (A[x2] < A[i])
+            if (A[x1] < A[x2 = i])
+                swap(x1, x2);
+    }
+}
 
-
-
-
-
-
+//TODO
+/*
+ * max3:递归+分治
+ * */
+void max3(int A[], int lo, int hi, int &x1, int &x2) {
+    if (lo + 2 == hi) {
+        max2(A, lo, hi, x1, x2);
+        return;
+    }//T(2) =1
+    if (lo + 3 == hi) {
+        max2(A, lo, hi, x1, x2);
+        return;
+    }//T(3)<=3
+    int mi = (lo + hi) / 2;//divide
+    int x1L, x2L;
+    max3(A, lo, mi, x1L, x2L);
+    int x1R, x2R;
+    max3(A, mi, hi, x1R, x2R);
+    if (A[x1L] > A[x1R]) {
+        x1 = x1L;
+        x2 = (A[x2L] > A[x1R]) ? x2L : x1R;
+    } else {
+        x1 = x1R;
+        x2 = (A[x1L] > A[x2R]) ? x1L : x2R;
+    }
+}
 
 
 
